d97ef6124eadc755ad2ad6fdeaaa172e1b0bea39
shanecranor <shanecranor@mines.edu>
2022-07-12 18:56:26 -0600

    refactor and clean up

--- components/spotify-component/spotify-component.jsx
+++ components/spotify-component/spotify-component.jsx
@@ -5,13 +5,16 @@
 // import Shrink from './images/down-left-and-up-right-to-center-solid.svg'
 import React from 'https://npm.tfl.dev/react'
 import Draggable from '../draggable/draggable.jsx'
+import SongInfo from '../song-info/song-info.jsx'
+import ToolTip from '../tooltip/tooltip.jsx'
 import ScopedStylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/scoped-stylesheet/scoped-stylesheet.js"
 import Stylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/stylesheet/stylesheet.js"
 import jumper from "https://tfl.dev/@truffle/utils@0.0.1/jumper/jumper.js"
 import { useEffect, useState } from 'https://npm.tfl.dev/react'
-const defaultModifier = { top: -43, right: 0, bottom: 0, left: 0, transition: "none" }
+
 function SpotifyComponent() {
   //set base dimensions
+  const defaultModifier = { top: -43, right: 0, bottom: 0, left: 10, transition: "none" }
   const base = { x: 415, y: 150 }
   const [dragProps, setDragProps] = useState(
     {
@@ -26,9 +29,10 @@ function SpotifyComponent() {
   const [collapsed, setCollapsed] = useState(false)
   const [trackPosition, setTrackPosition] = useState(0)
   //pull data from cloudflare worker
-  const refreshRate = 10000
+  const fetchRefreshMs = 10000
   const workerUrl = 'https://spotify-status-updater.shanecranor.workers.dev/'
   const orgID = 'shane'
+
   useEffect(() => {
     const overlayStates = {
       fullSize: { ...defaultModifier },
@@ -39,8 +43,7 @@ function SpotifyComponent() {
     if (toolTip) modifiers = overlayStates.toolTip
     if (collapsed) modifiers = overlayStates.collapsed
     setDragProps((old) => ({
-      ...old,
-      dimensions: { ...old.dimensions, modifiers: modifiers }
+      ...old, dimensions: { ...old.dimensions, modifiers: modifiers }
     }))
   }, [collapsed, toolTip])
 
@@ -55,99 +58,66 @@ function SpotifyComponent() {
     //TODO: queue a fetch to go off at the end of every song
     //fetch at the specified refresh rate
     const fetchInterval = setInterval(
-      () => {
-        fetchData().catch(console.error)
-      },
-      refreshRate
+      () => (fetchData().catch(console.error)),
+      fetchRefreshMs
     )
     return () => (clearInterval(fetchInterval))
   }, [])
-  //update time
-  const timeResolution = 1000
+
+  //update the progress bar based on system time
+  const progressBarRefreshMs = 1000
   useEffect(() => {
-    const timeUpdateInterval = setInterval(() => {
+    const progressUpdateInterval = setInterval(() => {
       if (!spotifyData) return
       if (spotifyData.is_playing) {
         setTrackPosition(spotifyData.position + (Date.now() - spotifyData.fetchTime))
       } else {
         setTrackPosition(spotifyData.position)
       }
-    }, timeResolution)
-    return () => (clearInterval(timeUpdateInterval))
+    }, progressBarRefreshMs)
+    return () => (clearInterval(progressUpdateInterval))
   }, [spotifyData])
+
   if (!spotifyData) return "loading"
   const progressDate = new Date(Math.min(trackPosition, spotifyData.length))
   const percentDone = trackPosition / spotifyData.length
   const collapsedTag = collapsed ? ' collapsed' : ''
   return (
     <aside style={{ background: "none" }}>
-      <Stylesheet url={new URL('./styles/root.css', import.meta.url)} />
       <style>{`html { overflow: hidden }`}</style>
       <Draggable
         dimensions={dragProps.dimensions}
         defaultPosition={dragProps.defaultPosition}>
-        <ScopedStylesheet url={new URL("styles/App.css", import.meta.url)}>
-          <div className={'spotify-component' + collapsedTag}
-            onClick={() => collapsed && setCollapsed(oldState => !oldState)}
-          >
-            <img
-              className={'album-art ' + collapsedTag}
-              src={spotifyData.images[0].url} alt='album cover'
-            />
-            <SongInfo
-              title={spotifyData.title}
-              link={spotifyData.link}
-              artists={spotifyData.artists}
-              length={spotifyData.length}
-              percentDone={percentDone}
-              progressDate={progressDate}
-            />
-            <div className='controls' >
-              <div className='help tooltip' onMouseOver={() => setToolTip(true)} onMouseOut={() => setToolTip(false)}
-                data-hover-text='what the streamer is currently listening to'>?</div>
-              <div className='minimize tooltip' onMouseOver={() => setToolTip(true)} onMouseOut={() => setToolTip(false)}
-                data-hover-text='shrink the spotify overlay'
-                onClick={() => setCollapsed(oldState => !oldState)}>‒</div>
-            </div>
+        <Stylesheet url={new URL("styles/App.css", import.meta.url)} />
+        <div className={'spotify-component' + collapsedTag}
+          onClick={() => collapsed && setCollapsed(oldState => !oldState)}
+        >
+          <img
+            className={'album-art ' + collapsedTag}
+            src={spotifyData.images[0].url} alt='album cover'
+          />
+          <SongInfo
+            spotifyData={spotifyData}
+            percentDone={percentDone}
+            progressDate={progressDate}
+          />
+          <div className='controls' >
+            <ToolTip className='help tooltip'
+              setHoverState={setToolTip}
+              hoverText='what the streamer is currently listening to'>
+              ?
+            </ToolTip>
+            <ToolTip className='minimize tooltip'
+              setHoverState={setToolTip}
+              hoverText='shrink the spotify overlay'
+              onClick={() => setCollapsed(oldState => !oldState)}>
+              ‒
+            </ToolTip>
           </div>
-        </ScopedStylesheet>
+        </div>
       </Draggable>
     </aside>
   )
 }
 
-function SongInfo({ title, link, artists, length, percentDone, progressDate }) {
-  function pad(n) {
-    return n < 10 ? `0${n}` : n
-  }
-  function formatDate(d) {
-    if (d.getUTCHours())
-      return `${d.getUTCHours()}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`
-    return `${d.getMinutes()}:${pad(d.getSeconds())}`
-  }
-  return (
-    <div className={'song-info '}>
-      <div className={'artist-title-container '}>
-        <div className='song-title'>
-          <a draggable="false" href={link} target="_blank" rel="noopener noreferrer">{title}</a>
-        </div>
-        <div className='artist-name'>
-          {artists.map(artist => (
-            <p key={artist}>
-              <a draggable="false" href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-                {artist.name}
-              </a>
-            </p>))}
-        </div>
-      </div>
-      <div className='progress-text-container'>
-        <div className='progress'>{formatDate(progressDate)}</div>
-        <div className='length'> {formatDate(new Date(length))}</div>
-      </div>
-      <div className='progress-bar-outer'>
-        <div className='progress-bar-inner' style={{ '--song-position': percentDone * 100 + '%' }}> </div>
-      </div>
-    </div>
-  )
-}
 export default SpotifyComponent
