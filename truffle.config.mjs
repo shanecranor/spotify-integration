73bce3cf46acf16279100d11857e7c047c613aef
Austin Hallock <austinhallock@gmail.com>
2022-07-20 13:03:02 -0600

    todo

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,4 +1,5 @@
 export default {
+  // TODO: rename to @truffle/spotify-now-listening for prod
   name: "@truffle/spotify-integration",
   version: "2.0.9",
   apiUrl: "https://mycelium.staging.bio/graphql",
