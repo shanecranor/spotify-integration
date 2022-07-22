6758aea77dc5bbd95e8e94c45edebf39d055ae8f
shanecranor <shanecranor@mines.edu>
2022-07-22 14:18:33 -0600

    fixed some minor typos that broke the deployed version

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   // TODO: rename to @truffle/spotify-now-listening for prod
   name: "@truffle/spotify-integration",
-  version: "2.2.5",
+  version: "2.2.8",
   apiUrl: "https://mycelium.staging.bio/graphql",
 };
