630ba848994fac16c4e6058cdad24d4df92ecc36
shanecranor <shanecranor@mines.edu>
2022-07-22 13:38:43 -0600

    fixed error where dragging wouldn't work after trying to drag a link off of the iframe

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   // TODO: rename to @truffle/spotify-now-listening for prod
   name: "@truffle/spotify-integration",
-  version: "2.0.9",
+  version: "2.2.5",
   apiUrl: "https://mycelium.staging.bio/graphql",
 };
